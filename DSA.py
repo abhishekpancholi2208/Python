d44107bffc40bb5729dc5ce15ebbd090a386bc68
Abhishek pancholi <abhishekpancholi22@gmail.com>
2022-10-12 15:11:35 +0530

    Update DSA.py

--- DSA.py
+++ DSA.py
@@ -1,1 +1,12 @@
-
+fname = input("Enter file name: ")
+count = 0
+total = 0
+fh = open(fname)
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    else:
+        count = count + 1
+        a = line.find('0')
+        n = float(line[a:])
+        total = total  + n
+print("Average spam confidence:",float(total/count))
