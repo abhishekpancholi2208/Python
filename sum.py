0a7bbb943bf73e72c7d13ccc3aae616e1bffd2a5
Zeeshan <zeeshan0703@outlook.com>
2022-10-16 15:26:51 +0530

    Added sum.py Created a basic programme that show how to sum two numbers in Python

---
+++ sum.py
@@ -0,0 +1,6 @@
+# This is a Python programme for the basic calculation of the user input
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+print("The sum of the given number is ", num1+num2)
